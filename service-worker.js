c2382af5d1a280776a69041707b04abc2779af24
Thomas Balouet <t.balouet@gmail.com>
2017-05-15 09:31:22 +0200

    rollback from previous commit work, separated in trunk branch

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,7 @@
 //in order to replace the former cache in the browser
 const CACHE_VERSION = 5;
 const CURRENT_CACHES = {
-  'PWA-Frame-Painter' : 'PWA-Frame-Painter-v' + CACHE_VERSION,
-  'post-message'      : 'post-message-cache-v' + CACHE_VERSION
+  'PWA-Frame-Painter' : 'PWA-Frame-Painter-v' + CACHE_VERSION
 };
 
 //List of the files representing the App shell
@@ -102,84 +101,4 @@ self.addEventListener('fetch', function(event) {
     }).catch((err) => {
       console.log("[ServiceWorker] Error in resolving fetch", err);
     }));
-});
-
-/**
- * Handles the message events from the client to save images
- * Inspired by https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/post-message
- * @param  {[type]} event) 
- * @return {[type]}        [description]
- */
-self.addEventListener('message', function(event) {
-  console.log('[ServiceWorker] Handling message event:', event);
-  var p = caches.open(CURRENT_CACHES['post-message']).then(function(cache) {
-    switch (event.data.command) {
-      case 'get':
-        caches.open(CURRENT_CACHES['post-message']).then(function(cache) {
-          return cache.match(event.data.url).then(function (response) {
-            console.log("[ServiceWorker] response from post-message for " + event.data.url, response);
-            event.ports[0].postMessage({
-              file : response
-            });
-          }).catch((err) => {
-            event.ports[0].postMessage({
-              error: err.toString()
-            });
-          });
-        }).catch((err) => {
-          event.ports[0].postMessage({
-            error: err.toString()
-          });
-        });
-        break;
-      
-      // This command adds a new request/response image pair to the cache.
-      case 'add':
-        var request = new Request(event.data.url, {mode: 'no-cors'});
-        var init = {
-            status     : 200,
-            statusText : "OK",
-            headers    : {'Content-Type': 'image/png'}
-        };
-
-        var response = new Response(event.data.file, init);
-        return cache.put(event.data.url, response).then(function() {
-          event.ports[0].postMessage({
-            error: null
-          });
-        });
-        break;
-
-      // This command removes a request/response pair from the cache (assuming it exists).
-      case 'delete':
-        return cache.delete(event.data.url).then(function(success) {
-          event.ports[0].postMessage({
-            error: (success ? null : 'Item was not found in the cache.')
-          });
-        });
-        break;
-
-      default:
-        // This will be handled by the outer .catch().
-        throw Error('Unknown command: ' + event.data.command);
-    }
-  }).catch(function(error) {
-    // If the promise rejects, handle it by returning a standardized error message to the controlled page.
-    console.error('[ServiceWorker] Message handling failed:', error);
-
-    event.ports[0].postMessage({
-      error: error.toString()
-    });
-  });
-
-  // Beginning in Chrome 51, event is an ExtendableMessageEvent, which supports
-  // the waitUntil() method for extending the lifetime of the event handler
-  // until the promise is resolved.
-  if ('waitUntil' in event) {
-    event.waitUntil(p);
-  }
-
-  // Without support for waitUntil(), there's a chance that if the promise chain
-  // takes "too long" to execute, the service worker might be automatically
-  // stopped before it's complete.
 });
